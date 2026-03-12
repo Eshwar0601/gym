fe225dd8b7faceb550435eca8f577a2ef2fae858
Eshwar0601 <852eshwar@gmail.com>
2026-03-12 22:47:12 +0530

    cors filter changes

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -32,6 +33,7 @@ mongoose.connect('mongodb+srv://admin:1234@cluster0.bdpjfdh.mongodb.net/?appName
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
